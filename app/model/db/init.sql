e156818e7dc7d7244b037b65a3404879f08f32a5
chemiX <iam@chemix.cz>
2014-11-08 14:43:42 +0100

    update sql init script

--- app/model/db/init.sql
+++ app/model/db/init.sql
@@ -6,16 +6,75 @@
 -- FLUSH PRIVILEGES;
 
 
-
--- Adminer 4.1.0 MySQL dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP TABLE IF EXISTS `expirience_category`;
-CREATE TABLE `expirience_category` (
+
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+
+
+-- System Users
+-- default user  architect : kreslo
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `password` char(60) COLLATE utf8_czech_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `role` varchar(30) COLLATE utf8_czech_ci NOT NULL DEFAULT 'user',
+  `active` char(1) COLLATE utf8_czech_ci NOT NULL DEFAULT '1',
+  `name` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  `avatar` varchar(250) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `active`, `name`, `avatar`) VALUES
+  (1, 'architect',  '$2y$10$K.JEAIhI/bmk2Kas2uxFi.3Y.qJ6LZNw44X5k9Lq81R27wgcZqsSu', 'info@aprila.cz', 'root', '1',  'Architect',  '');
+
+DROP TABLE IF EXISTS `users_password_reset`;
+CREATE TABLE `users_password_reset` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `salt` char(32) COLLATE utf8_czech_ci NOT NULL,
+  `token` char(64) COLLATE utf8_czech_ci NOT NULL,
+  `userId` int(10) unsigned NOT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userId` (`userId`),
+  CONSTRAINT `userId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+
+-- change email
+ALTER TABLE `users`
+ADD `change_email_requested` datetime NOT NULL,
+ADD `change_email_tokenOne` char(60) COLLATE 'utf8_czech_ci' NOT NULL AFTER `change_email_requested`,
+ADD `change_email_tokenTwo` char(60) COLLATE 'utf8_czech_ci' NOT NULL AFTER `change_email_tokenOne`,
+COMMENT='';
+
+ALTER TABLE `users`
+ADD `change_email` varchar(255) COLLATE 'utf8_czech_ci' NOT NULL AFTER `avatar`,
+COMMENT='';
+
+
+
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+
+
+
+-- My Life Style - Day and Experience
+
+
+DROP TABLE IF EXISTS `experience_category`;
+CREATE TABLE `experience_category` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -28,7 +87,7 @@ CREATE TABLE `day` (
   `date` date NOT NULL,
   `start_time` datetime DEFAULT NULL,
   `mood` int(1) unsigned DEFAULT NULL,
-  `expirience_time` datetime DEFAULT NULL,
+  `experience_time` datetime DEFAULT NULL,
   `end_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `date` (`date`,`user_id`),
@@ -36,25 +95,32 @@ CREATE TABLE `day` (
   CONSTRAINT `day_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
-DROP TABLE IF EXISTS `expirience`;
-CREATE TABLE `expirience` (
+DROP TABLE IF EXISTS `experience`;
+CREATE TABLE `experience` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `day_id` int(10) unsigned DEFAULT NULL,
   `description` varchar(255) DEFAULT '',
-  `expirience_id` int(10) unsigned DEFAULT NULL,
+  `experience_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `day_id` (`day_id`),
-  KEY `expirience_id` (`expirience_id`),
-  CONSTRAINT `expirience_ibfk_2` FOREIGN KEY (`expirience_id`) REFERENCES `expirience` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `expirience_ibfk_1` FOREIGN KEY (`day_id`) REFERENCES `day` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `experience_id` (`experience_id`),
+  CONSTRAINT `experience_ibfk_2` FOREIGN KEY (`experience_id`) REFERENCES `experience` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `experience_ibfk_1` FOREIGN KEY (`day_id`) REFERENCES `day` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET foreign_key_checks = 1;
 
 
+
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------
+
+
+
 -- Insert default categories
 
-INSERT INTO `expirience_category` (`id`, `name`) VALUES
+INSERT INTO `experience_category` (`id`, `name`) VALUES
 (1,	'Sportovní aktivita'),
 (2,	'Večírek'),
 (3,	'Setkání se s přáteli'),
