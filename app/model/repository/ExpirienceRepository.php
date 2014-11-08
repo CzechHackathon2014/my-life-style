371a0cd3d850ad92a9691f1b6c6a20de89c631a3
Martin Chapcak <chapcak.m@gmail.com>
2014-11-08 14:37:39 +0100

    New repository for expirience table

---
+++ app/model/repository/ExpirienceRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Honza Cerny (http://honzacerny.com)
+ */
+
+namespace App\Model;
+
+use Aprila\Model\BaseRepository;
+use Nette\Utils\Strings;
+
+class ExpirienceRepository extends BaseRepository
+{
+	/** @var string */
+	public $table = 'expirience';
+
+
+
+
+}
